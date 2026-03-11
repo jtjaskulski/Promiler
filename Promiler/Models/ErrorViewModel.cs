051a5811893be6deebc3f66a2006d5a4e9de7e6f
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-11 17:49:37 +0100

    Init of project

---
+++ Promiler/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Promiler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
